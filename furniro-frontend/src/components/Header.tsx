f85e738b5b8521f856420ce2e706c6e472e9548c
uttam pun <uttampun50@gmail.com>
2024-06-23 22:43:24 +0545

    adding bootstrap class

--- furniro-frontend/src/components/Header.tsx
+++ furniro-frontend/src/components/Header.tsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router-dom"
-import React from 'react'
+import { Link } from "react-router-dom";
+import Logo from '../assets/images/logo.png'
 
 function Header() {
   return (
     <div>
-        <header>
-             <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/signup'>Signup</Link></li>
-             </ul>
-        </header>
+      <header>
+        <div className="header-row container-lg mx-auto row justify-space-between align-items-center">
+          <div className="header-logo d-flex col-lg-3">
+            <img src={Logo}  alt="logo" style={{width:'50px', height: '50px', objectFit: 'contain'}} /><h1>Furniro</h1>
+          </div>
+          <div className="header-login-row col-lg-3">
+            <nav>
+              <ul className="d-flex list-none">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Signup</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="header-login-signup col-lg-6">
+            cart
+          </div>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
