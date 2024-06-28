fc9ae6bc8683be4e2e03d0f6224e6e8aeb7d14e4
uttampun <uttampun50@gmail.com>
2024-06-28 09:43:29 +0545

    added new signup form

--- furniro-frontend/src/components/Footer.tsx
+++ furniro-frontend/src/components/Footer.tsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
-function Footer() {
+
+interface footerLinks {
+  links: {
+    name: string,
+    path: string
+  }[]
+}
+
+function Footer(props: footerLinks) {
   return (
-    <div>Footer</div>
+    <div className="border-t-slate-400 border-2">
+      <footer>
+        <div className="footer-row max-w-[1440px]  mx-auto flex justify-between py-14">
+          <div className="footer_title p-2">
+            <div className="title">
+              <h1 className="font-bold text-2xl">Furnio .</h1>
+            </div>
+            <div className="footer_address my-4">
+              <address className="text-lg font-normal not-italic text-gray-400">
+                400 University Drive Suite 200 Coral<br></br>
+                Gables,
+                FL 33134 USA
+              </address>
+            </div>
+
+          </div>
+
+          <div className="links">
+            <div className="links_title">
+              <h2 className="text-slate-400 font-medium text-xl">Link</h2>
+            </div>
+            <div className="links">
+              <ul className="grid gap-y-6 my-4">
+                {
+                  props.links.map((link, index) => (
+                    <li key={index}>
+                      <Link to={link.path} className="text-xl font-medium text-decoration-none text-dark">
+                        {link.name}
+                      </Link>
+                    </li>
+                  ))
+                }
+
+              </ul>
+            </div>
+          </div>
+
+          <div className="help">
+            <div className="help_title">
+              <h2 className="text-slate-400 font-medium text-xl">Help</h2>
+            </div>
+            <div className="help_links my-4">
+              <ul className="grid gap-y-6">
+                <li><span className="text-xl font-medium text-decoration-none text-dark">Payment</span></li>
+                <li><span className="text-xl font-medium text-decoration-none text-dark">Returns</span></li>
+                <li><span className="text-xl font-medium text-decoration-none text-dark">Privacy Policies</span></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="newsletter">
+            <div className="news_letter ">
+              <h2 className="text-slate-400 font-medium text-xl">Newsletter</h2>
+            </div>
+            <div className="newsletter_form my-4 flex gap-x-2">
+                <input name="email_address" placeholder="Enter Your Email Address" className="border-t-0 outline-none border-black border-l-0 border-r-0 border-2 "/>
+                <button className="text-lg font-bold border-2 border-black border-l-0 border-r-0 border-t-0">Subscribe</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="furnio_copyrights py-10 max-w-[1440px]  mx-auto border-t-2">
+           <strong>2024 furnio. All rights reserved</strong>
+        </div>
+      </footer>
+    </div>
   )
 }
 
--- furniro-frontend/src/page/Login.tsx
+++ furniro-frontend/src/page/Login.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import Backgroundpic from '../components/Backgroundpic'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 
@@ -23,11 +25,11 @@ function Login() {
           <form className="max-w-sm mx-auto" method='POST'>
             <div className="mb-5">
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-              <input type="email" id="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400"  required />
+              <input type="email" id="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none block w-full p-2.5 placeholder-gray-400"  required />
             </div>
             <div className="mb-5">
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
-              <input type="password" id="password" name='password' className="bg-gray-100 border  text-gray-900 text-sm rounded-lg  block w-full p-2.5 placeholder-gray-400" required />
+              <input type="password" id="password" name='password' className="bg-gray-100 border  border-gray-300 outline-none  text-gray-900 text-sm rounded-lg  block w-full p-2.5 placeholder-gray-400" required />
             </div>
             <div className="flex items-start mb-5">
               <div className="flex items-center h-5">
@@ -35,11 +37,16 @@ function Login() {
               </div>
               <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
             </div>
+            <div className="flex-button flex gap-x-4">
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-          </form>
+            <Link to='/signup' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</Link>
 
+            </div>
+           
+          </form>
         </div>
       </div>
+      <Footer links={header}/>
     </div>
   )
 }
