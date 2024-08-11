b99a35c33211db40d350b3ca3155000d2534ce68
uttampun <uttampun50@gmail.com>
2024-08-11 09:21:22 +0545

    adding redux toolkit and products table create

---
+++ furniro-backend/database/migrations/2024_08_11_032438_create_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('SKU');
+            $table->decimal('price');
+            $table->foreignId('category_id')->constrained('products_categories')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('discount_id')->constrained('product_discounts')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('quantity_id')->constrained('product_quantities')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
